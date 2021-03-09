7d7dbf09949ae8aa33feb1945bfdfc8171e897af
Sara Nielsen <sara345a@stud.kea.dk>
2021-03-09 14:29:08 +0100

    exercise done

--- follow.js
+++ follow.js
@@ -3,14 +3,17 @@ window.addEventListener("DOMContentLoaded", loadSVG);
 function loadSVG() {
     console.log("load the SVG");
 
+ 
+
     fetch("car_and_road.svg")
     .then( response => response.text() )
     .then( svgData => {
         console.log("SVG loaded");
-
         // TODO: put the SVG into the DOM
+        document.querySelector("#thesvg").innerHTML = svgData;
 
         // TODO: Start the animation
+        runAnimation();
 
     })
 }
@@ -23,4 +26,5 @@ function runAnimation() {
     console.log("animate");
 
     // TODO: Build animation ...
+    document.querySelector("#car").classList.add("move");
 }
